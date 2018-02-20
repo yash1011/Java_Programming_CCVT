03597a3887bd269df150296617b606b28837c44b
Yash Saraswat <Yash Saraswat@LAPTOP-M4VKME1S>
2018-02-20 11:51:44 +0530

    Mandir vahi banayenge

--- CCVT_Code_Distribute/src/com/rt/cloning/TestClass.java
+++ CCVT_Code_Distribute/src/com/rt/cloning/TestClass.java
@@ -8,17 +8,17 @@ public class TestClass {
 	 */
 	public static void main(String[] args) throws CloneNotSupportedException {
 		// TODO Auto-generated method stub
-	Department d1=new Department(7, "HR");
-	Employee e1=new Employee(1, "Ram",d1);
+	Department d1=new Department(7, "PR");
+	Employee e1=new Employee(1, "Yash",d1);
 	Employee e2= e1.clone();
 	
 
 	System.out.println(e1);
 	System.out.println(e2);
 //	e2.setEid(3);
-	e2.setEname("Shyam");
+	e2.setEname("Raja");
 	e2.getDep().setDid(8);
-	e2.getDep().setDname("Acc");
+	e2.getDep().setDname("HR");
 	System.out.println(e1);
 	System.out.println(e2);
 //new Employee().getEid();
